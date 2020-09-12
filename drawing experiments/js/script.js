694bf2303784876e41f72ced165d0dda7d02c864
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-12 00:25:21 -0400

    I DREW A BEAR

--- drawing experiments/js/script.js
+++ drawing experiments/js/script.js
@@ -10,6 +10,36 @@ Here is a description of this template p5 project.
 // Description of setup() goes here.
 function setup() {
 
+  createCanvas(500, 500);
+
+  background(500, 500, 500);
+
+  let c = color(128, 64, 32);
+
+  // head
+  fill(c);
+  ellipse(100, 125, 125, 140);
+  ellipse(400, 125, 125, 140);
+  fill(256, 128, 64);
+  ellipse(400, 125, 62, 70);
+  ellipse(100, 125, 62, 70);
+  fill(c);
+  ellipse(250, 250, 450, 375);
+
+
+  // eyes
+  ellipseMode(CORNER);
+  fill(500, 500, 500);
+  ellipse(100, 150, 120, 120);
+  ellipse(280, 150, 120, 120);
+  fill(0);
+  ellipse(100, 150, 100, 100);
+  ellipse(280, 150, 100, 100);
+
+  //nose
+  ellipse(225, 300, 50, 25);
+
+
 }
 
 // draw()
