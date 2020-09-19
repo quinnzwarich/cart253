80af1688ef1448e13795edfa8d2735c922e624b8
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-18 20:32:06 -0400

    variable animations

--- variables experiments/js/script.js
+++ variables experiments/js/script.js
@@ -5,18 +5,27 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let backgroundShade = 0;
+let circleSize = 200;
+let circleX = 0;
+let circleY = 500;
+let circleSpeed = 2;
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 1000);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(255, 0, 0);
-  rectMode(CENTER);
-  rect(width / 2, height / 2, 100, 100);
+  backgroundShade = backgroundShade + 0.5;
+  background(backgroundShade);
+  circleX = circleX + circleSpeed;
+  circleSpeed = circleSpeed + 0.25;
+  noStroke();
+  ellipse(circleX, circleY, circleSize);
 }
