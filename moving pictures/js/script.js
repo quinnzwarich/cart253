6d2dfa27713d1db876fd7a87d5916f4dcb0eb8be
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-21 12:19:15 -0400

    completed the second activity

--- moving pictures/js/script.js
+++ moving pictures/js/script.js
@@ -5,16 +5,54 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let backgroundShade = 0;
+
+let circle = {
+  x: 0,
+  y: 500,
+  size: 250,
+  speed: 2,
+}
+
+let circle2 = {
+  x: 1000,
+  y: 500,
+  size: 200,
+  speed: 2,
+}
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(1000, 1000);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundShade, 0, 0);
+  backgroundShade = map(circle.x, 0, width/2, 0, 255);
+
+
+  circle.x = circle.x + circle.speed;
+  circle.y = map(circle.x, 0, width/2, 500, 375);
+  circle.size = circle.size - circle.speed;
+  circle.x = constrain(circle.x, 0, width/2);
+
+
+  circle2.x = circle2.x - circle2.speed;
+  circle2.y = map(circle2.x, width/2, width, 625, 500);
+  circle2.size = circle2.size - circle2.speed;
+  circle2.x = constrain(circle2.x, width/2, width);
+
+  noStroke();
+  fill(255, 255, 255, 200);
+  ellipse(circle.x, circle.y, circle.size);
+
+  noStroke();
+  fill(255, 255, 255, 225);
+  ellipse(circle2.x, circle2.y, circle2.size);
 
 }
--- variables experiments/js/script.js
+++ variables experiments/js/script.js
@@ -8,7 +8,7 @@ Here is a description of this template p5 project.
 let backgroundShade = 0;
 
 let circle = {
-  x: 500,
+  x: 0,
   y: 500,
   size: 200,
   speed: 2,
@@ -28,12 +28,14 @@ function setup() {
 function draw() {
   background(backgroundShade);
 
-  circle.speed = random(-5, 5);
+
   circle.x = circle.x + circle.speed;
-  circle.y = circle.y - circle.speed;
+
+  circle.size = map(mouseY, 0, height, 100, 500);
+  circle.x = constrain(circle.x, 0, width)
+  circle.fill = map(circle.x, 0, width, 0, 255);
 
   noStroke();
-  circle.fill = random(0, 255);
   fill(circle.fill);
   ellipse(circle.x, circle.y, circle.size);
 
