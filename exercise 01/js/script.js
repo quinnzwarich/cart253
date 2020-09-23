9220b793ed42953e64b9256cdc7068e781002bf4
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-22 23:36:49 -0400

    E1: added text

--- exercise 01/js/script.js
+++ exercise 01/js/script.js
@@ -2,9 +2,9 @@
 exercise 01
 Quinn Zwarich
 
-The program has changed since a bit since it's first inception.
-
-
+The program has changed since a bit since it's first inception. There are now 3 circles that dialate and sort of rebound
+when they become too small. My intention in designing was to have as much feedback between parameters as possible
+(without everything blowing up). The cursor's role revolves around this, it controls the speed and rate at which feedback occurs.
 
 **************************************************/
 
@@ -47,7 +47,7 @@ function setup() {
   createCanvas(1000, 1000);
 }
 
-// sin and cos are used to add cyclical movement that remains within the bounds of the canvas
+// sin and cos are basically just here to make things spicy
 
 function draw() {
 background(bg.r, bg.g, bg.b);
@@ -60,25 +60,15 @@ circle3.y = circle.y + sin(speed) * multiplier;
 circle3.size = circle3.size + cos(speed) * (circle.x / 50);
 
 circle.x = map(circle2.x, 250, width, 400, 0);
-//circle.x = circle.x + circle.speed;
 circle.y = map(circle2.y, 100, height, 400, 0);
-//circle.y = circle.y + circle.speed;
 
 circle2.x = map(circle3.x, 400, 0, 100, width);
-//circle2.x = circle2.x + circle2.speed;
 circle2.y = map(circle3.y, 400, 0, 400, height);
-//circle2.y = circle2.y + circle2.speed
 
 circle3.x = map(circle.x, 100, width, 250, 0);
-//circle3.x = circle3.x + circle3.speed;
 circle3.y = map(circle.y, 400, 0, 100, height);
-//circle3.y = circle3.y + circle3.speed;
 
- // size controls both the shade of the background and of the circles but is inverted
- // the screen begins with a white background, undergoes a transition consisting of neutral grey tones,
- // then ends with a return to white negative space as the circles become larger than the background
-
- // the interference patterns go through many stages, as a result of this it sometimes ends with black
+// background colour and the colour of circles receive inverted modulation for contrast
 
 multiplier = map(mouseX, 0, width, 1, 500);
 speed = map(mouseY, 0, height, 1, 15);
@@ -87,10 +77,8 @@ bg.r = map(circle.size, 200, 1000, 255, 0);
 colour.b = map(circle2.y, 1000, 0, 0, 255);
 bg.b = map(circle3.y, 1000, 0, 255, 0);
 
-
-
-
   fill(colour.r, colour.g, colour.b);
+
   noStroke();
 
   ellipse(circle.x, circle.y, circle.size);
