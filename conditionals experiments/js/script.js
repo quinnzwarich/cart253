81085e9e29c599d7e712f69f2b7806f85dab0e06
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-25 17:35:49 -0400

    looked at various conditions and methods of looping and counting

--- conditionals experiments/js/script.js
+++ conditionals experiments/js/script.js
@@ -5,16 +5,28 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-
+let caterpillar = {
+  x: 0,
+  y: 250,
+  segmentSize: 5
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+function setup() {
+  createCanvas(500, 500);
+}
 
+function draw() {
+  background(0);
+  noStroke();
+  fill(100,200,100);
+
+  let x = caterpillar.x;
+  let runSegments = 500;
+  let segmentsDrawn = 0;
+
+  while (segmentsDrawn < runSegments) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 4;
+    segmentsDrawn = segmentsDrawn + 1;
+  }
 }
