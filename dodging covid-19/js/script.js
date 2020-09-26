c8f637455b91d7100b3bd227f2667a250e5d899e
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-26 17:01:09 -0400

    completed the covid 19 activity, made it so that static increased with proximity to the virus

--- dodging covid-19/js/script.js
+++ dodging covid-19/js/script.js
@@ -1,20 +1,81 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+dodging covid-19
+Quinn Zwarich
 
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0
+  }
+}
 
+let user = {
+  x: 250,
+  y: 250,
+  size: 100,
+  fill: 255
+}
+
+let static = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  covid19.y = random(0, height);
+  covid19.vx = covid19.speed;
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  // covid19 movement
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0, height);
+  }
+
+  // user movement
+  noCursor();
+  user.x = mouseX;
+  user.y = mouseY;
+
+  // check for covid19
+  let d = dist(user.x, user.y, covid19.x, covid19.y);
+  if (d < covid19.size/2 + user.size/2) {
+    noLoop();
+  }
+
+  // display static
+    static = map(d, 0, width, 500, 0);
+
+    for (let i = 0; i < static; i++) {
+      let x = random(0, width);
+      let y = random(0, height);
+      stroke(255);
+      point(x,y);
+    }
+
+  // display covid19
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+  // display user
+  fill(user.fill);
+  ellipse(user.x, user.y, user.size);
 }
