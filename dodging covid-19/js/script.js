c464c5a8fa8d96974ab6359de1f6c6b51494ef80
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-28 18:30:08 -0400

    E2: changed movement of chasing ellipse so that it becomes more random the farther away it is from the other ellipse, added reflected curves

--- dodging covid-19/js/script.js
+++ dodging covid-19/js/script.js
@@ -8,10 +8,10 @@ Here is a description of this template p5 project.
 let covid19 = {
   x: 0,
   y: 0,
-  size: 150,
+  size: 100,
   vx: 0,
   vy: 0,
-  speed: 5,
+  speed: 2,
   fill: {
     r: 255,
     g: 255,
@@ -48,34 +48,55 @@ function setup() {
 function draw() {
   background(0);
 
-  // covid19 movement
-  //covid19.x = covid19.x + covid19.vx;
-  //covid19.y = covid19.y + covid19.vy;
-
-  //if (user.x > width/2) {
-  //  if (covid19.x > width || covid19.x < 0) {
-  //    covid19.x = width;
-  //    covid19.vx = covid19.vx + 1;
-  //    covid19.vx = -(covid19.vx);
-  //    covid19.y = user.y;
-  //  }
-  //}
-
-  //if (user.x < width/2) {
-  //  if (covid19.x > width || covid19.x < 0) {
-  //    covid19.x = 0;
-  //    covid19.vx = covid19.vx + 1;
-  //    covid19.vx = -(covid19.vx);
-  //    covid19.y = user.y;
-  //  }
-  //}
-
-
-
   // user movement
   noCursor();
   user.x = mouseX;
   user.y = mouseY;
+
+  // covid19 movement
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  if (covid19.x > width) {
+    covid19.x = random(0, width/2);
+  }
+
+  if (covid19.x < 0) {
+    covid19.x = random(width/2, width);
+  }
+
+  if (covid19.y > height) {
+    covid19.y = random(0, height/2);
+  }
+
+  if (covid19.y < 0) {
+    covid19.y = random(height/2, height);
+  }
+
+  let r = random();
+
+  let xReact = map(user.x, 0, width, 0, 1);
+  let yReact = map(user.y, 0, height, 0, 1);
+
+  if (r < xReact) {
+    covid19.vx = random(-3, 3);
+  }
+  else if (covid19.x < user.x) {
+    covid19.vx = 3;
+  }
+  else if (covid19.x > user.x) {
+    covid19.vx = -3;
+  }
+
+  if (r < yReact) {
+    covid19.vy = random(-3, 3);
+  }
+  else if (covid19.y < user.y) {
+    covid19.vy = 3;
+  }
+  else if (covid19.y > user.y) {
+    covid19.vy = -3;
+  }
 
   // check for covid19
   let d = dist(user.x, user.y, covid19.x, covid19.y);
@@ -91,13 +112,16 @@ function draw() {
         let x1 = map(tentacles.drawn, 0, tentacles.total, width, 0);
         let y1 = map(tentacles.drawn, 0, tentacles.total, height, 0);
 
-        tightness = map(covid19.x, 0, width, -5, 5);
+        tightness = map(user.x, 0, width, -5, 5);
 
         push();
         curveTightness(tightness);
         stroke(currentFill);
         fill(currentFill, currentFill, currentFill/2, currentFill);
-        curve(x, y, covid19.x, covid19.y, x1, y1, covid19.x * (tentacles.spacing*PI), covid19.y * (tentacles.spacing*PI));
+        curve(x, y, user.x, user.y, x1, y1, user.x * (tentacles.spacing*PI), user.y * (tentacles.spacing*PI));
+        curve(x, y, user.x, user.y, x, y1, user.x * (tentacles.spacing*PI), user.y * (tentacles.spacing*PI));
+        curve(x, y, user.x, user.y, x1, y, user.x * (tentacles.spacing*PI), user.y * (tentacles.spacing*PI));
+        curve(x, y, user.x, user.y, x, y, user.x * (tentacles.spacing*PI), user.y * (tentacles.spacing*PI));
         pop();
 
         tentacles.drawn = tentacles.drawn + 1;
@@ -111,21 +135,20 @@ function draw() {
 
       }
 
-    if (!(tentacles.drawn < tentacles.total)) {
+  if (!(tentacles.drawn < tentacles.total)) {
+
       tentacles.drawn = 0;
-    }
+
+      }
 
     // display user
     fill(user.fill);
+    ellipse(covid19.x, covid19.y, covid19.size);
+
+    // display covid19
+    noStroke();
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
     ellipse(user.x, user.y, user.size);
-
-angle = angle + 0.2;
-
-      fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-      translate(width/2, height/2);
-      rotate(2*PI);
-      noStroke();
-      ellipse(covid19.x, covid19.y, covid19.size);
 
 
 
