7f5b3266d4d026eab2b7312e1a99aa399edb7da7
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-09-29 22:44:26 -0400

    E2: added comments and improved how the user drags the moth

--- mothsimulator/js/script.js
+++ mothsimulator/js/script.js
@@ -1,8 +1,8 @@
 /**************************************************
-dodging covid-19
+mothsimulator
 Quinn Zwarich
 
-Here is a description of this template p5 project.
+The simulation depicts a moth wandering towards a firefly, if it touches the firefly it gets zapped and the simulation ends.
 **************************************************/
 
 let moth = {
@@ -11,13 +11,13 @@ let moth = {
   size: 100,
   vx: 0,
   vy: 0,
-}
+};
 
-let tentacles = {
+let wings = {
   total: 8,
   spacing: 0,
   drawn: 0,
-}
+};
 
 let firefly = {
   x: 250,
@@ -26,58 +26,56 @@ let firefly = {
   fill: {
     r: 255,
     g: 255,
-    b: 125
-  }
-}
+    b: 125,
+  },
+};
 
 let user = {
   x: 0,
   y: 0,
-}
+  size: 100,
+};
 
 let mth;
 
 function preload() {
-  mth = loadImage('assets/moth.jpg');
+  mth = loadImage("assets/images/moth.jpg");
 }
-
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-mth.loadPixels();
-
-  moth.x = width/2;
+  moth.x = width / 2;
   moth.y = height;
 
-  firefly.x = width/2;
-  firefly.y = height/3;
-
+  firefly.x = width / 2;
+  firefly.y = height / 3;
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
   background(0);
 
-  // firefly movement
-  noCursor();
+  // user interaction
+  // allows moth to be dragged away from the firefly
+  // affects the positions of the firefly wings
+  if (mouseIsPressed) {
+    user.x = mouseX;
+    user.y = mouseY;
+  }
 
-if (mouseIsPressed) {
-  user.x = mouseX;
-  user.y = mouseY;
-}
+  let d = dist(user.x, user.y, moth.x, moth.y);
+
+  if (mouseIsPressed && d < user.size / 2 + moth.size / 2) {
+    moth.x = user.x;
+    moth.y = user.y;
+  }
 
   // moth movement
+  // the idea is that the farther the moth is from the firefly, the less likely it is to be interested
+  // this was more effective in a previous iteration in which the user controlled the firefly
+  // the use of random still makes for very moth like movement
   moth.x = moth.x + moth.vx;
   moth.y = moth.y + moth.vy;
-
-if (mouseIsPressed) {
-  moth.x = user.x;
-  moth.y = user.y;
-}
 
   let r1 = random();
 
@@ -86,100 +84,129 @@ if (mouseIsPressed) {
 
   if (r1 < xReact) {
     moth.vx = random(-1, 1);
-  }
-  else if (moth.x < firefly.x) {
+  } else if (moth.x < firefly.x) {
     moth.vx = 1;
-  }
-  else if (moth.x > firefly.x) {
+  } else if (moth.x > firefly.x) {
     moth.vx = -1;
   }
 
   if (r1 < yReact) {
     moth.vy = random(-1, 1);
-  }
-  else if (moth.y < firefly.y) {
+  } else if (moth.y < firefly.y) {
     moth.vy = 1;
-  }
-  else if (moth.y > firefly.y) {
+  } else if (moth.y > firefly.y) {
     moth.vy = -1;
   }
 
   // check for moth
-  let d = dist(firefly.x, firefly.y, moth.x, moth.y);
-  if (d < moth.size/2 + firefly.size/2) {
+  let d1 = dist(firefly.x, firefly.y, moth.x, moth.y);
+  if (d1 < moth.size / 2 + firefly.size / 2) {
     noLoop();
   }
 
-    while (tentacles.drawn < tentacles.total) {
+  // draw the firefly wings
+  // a whole lot is going on here, I can't say I completely understand what I did
+  // the idea here is that the number of of wings is gradation and curvature of each controlled
+  // as well, i attempted to draw each one PI/8 radians apart relative to each set
+  // there are four sets, each being reflected across a different axis
+  // (pay special attention to the 5th and 6th arguments of each curve drawing to see this)
+  while (wings.drawn < wings.total) {
+    let currentFill = map(wings.drawn, 0, wings.total, 0, 255);
+    let x = map(wings.drawn, 0, wings.total, 0, width);
+    let y = map(wings.drawn, 0, wings.total, 0, height);
+    let x1 = map(wings.drawn, 0, wings.total, width, 0);
+    let y1 = map(wings.drawn, 0, wings.total, height, 0);
 
-        let currentFill = map(tentacles.drawn, 0, tentacles.total, 0, 255);
-        let x = map(tentacles.drawn, 0, tentacles.total, 0, width);
-        let y = map(tentacles.drawn, 0, tentacles.total, 0, height);
-        let x1 = map(tentacles.drawn, 0, tentacles.total, width, 0);
-        let y1 = map(tentacles.drawn, 0, tentacles.total, height, 0);
+    tightness = map(firefly.x, 0, width, -5, 5);
 
-        tightness = map(firefly.x, 0, width, -5, 5);
+    // animation of the wings
+    if (user.x > x) {
+      firefly.vx = 15;
+    } else if (user.x < x) {
+      firefly.vx = -15;
+    }
+    if (user.x < x1) {
+      firefly.vx = 15;
+    } else if (user.x > x) {
+      firefly.vx = -15;
+    }
+    if (user.y > y) {
+      firefly.vy = 15;
+    } else if (user.y < y) {
+      firefly.vy = -15;
+    }
+    if (user.y < y1) {
+      firefly.vy = 15;
+    } else if (user.x > y1) {
+      firefly.vy = -15;
+    }
 
-        if (user.x > x) {
-           firefly.vx = 15;
-         }
-         else if (user.x < x) {
-           firefly.vx = -15;
-         }
-           if (user.x < x1) {
-              firefly.vx = 15;
-            }
-            else if (user.x > x) {
-              firefly.vx = -15;
-            }
-              if (user.y > y) {
-                 firefly.vy = 15;
-               }
-               else if (user.y < y) {
-                 firefly.vy = -15;
-               }
-                 if (user.y < y1) {
-                    firefly.vy = 15;
-                  }
-                  else if (user.x > y1) {
-                    firefly.vy = -15;
-                  }
+    push();
+    curveTightness(tightness);
+    stroke(currentFill);
+    fill(currentFill, currentFill, currentFill / 2, currentFill);
+    curve(
+      x,
+      y,
+      firefly.x,
+      firefly.y,
+      x1 + firefly.vx,
+      y1 + firefly.vy,
+      firefly.x * (wings.spacing * PI),
+      firefly.y * (wings.spacing * PI)
+    );
+    curve(
+      x,
+      y,
+      firefly.x,
+      firefly.y,
+      x + firefly.vx,
+      y1 + firefly.vy,
+      firefly.x * (wings.spacing * PI),
+      firefly.y * (wings.spacing * PI)
+    );
+    curve(
+      x,
+      y,
+      firefly.x,
+      firefly.y,
+      x1 + firefly.vx,
+      y + firefly.vy,
+      firefly.x * (wings.spacing * PI),
+      firefly.y * (wings.spacing * PI)
+    );
+    curve(
+      x,
+      y,
+      firefly.x,
+      firefly.y,
+      x + firefly.vx,
+      y + firefly.vy,
+      firefly.x * (wings.spacing * PI),
+      firefly.y * (wings.spacing * PI)
+    );
+    pop();
 
-        push();
-        curveTightness(tightness);
-        stroke(currentFill);
-        fill(currentFill, currentFill, currentFill/2, currentFill);
-        curve(x, y, firefly.x, firefly.y, x1 + firefly.vx, y1 + firefly.vy, firefly.x * (tentacles.spacing*PI), firefly.y * (tentacles.spacing*PI));
-        curve(x, y, firefly.x, firefly.y, x + firefly.vx, y1 + firefly.vy, firefly.x * (tentacles.spacing*PI), firefly.y * (tentacles.spacing*PI));
-        curve(x, y, firefly.x, firefly.y, x1 + firefly.vx, y + firefly.vy, firefly.x * (tentacles.spacing*PI), firefly.y * (tentacles.spacing*PI));
-        curve(x, y, firefly.x, firefly.y, x + firefly.vx, y + firefly.vy, firefly.x * (tentacles.spacing*PI), firefly.y * (tentacles.spacing*PI));
-        pop();
+    wings.drawn = wings.drawn + 1;
+  }
 
-        tentacles.drawn = tentacles.drawn + 1;
+  if (wings.drawn + 1) {
+    wings.spacing = wings.spacing + 22.5;
+    wings.spacing = wings.spacing / 180;
+  }
 
-      }
+  if (!(wings.drawn < wings.total)) {
+    wings.drawn = 0;
+  }
 
-  if (tentacles.drawn + 1) {
+  // display moth
+  push();
+  imageMode(CENTER);
+  image(mth, moth.x, moth.y, moth.size, moth.size);
+  pop();
 
-      tentacles.spacing = tentacles.spacing + 22.5;
-      tentacles.spacing = tentacles.spacing / 180;
-
-      }
-
-  if (!(tentacles.drawn < tentacles.total)) {
-
-      tentacles.drawn = 0;
-
-      }
-
-    // display moth
-    image(mth, moth.x, moth.y, moth.size, moth.size);
-
-    // display firefly
-    noStroke();
-    fill(firefly.fill.r, firefly.fill.g, firefly.fill.b);
-    ellipse(firefly.x, firefly.y, firefly.size);
-
-
-
+  // display firefly
+  noStroke();
+  fill(firefly.fill.r, firefly.fill.g, firefly.fill.b);
+  ellipse(firefly.x, firefly.y, firefly.size);
 }
