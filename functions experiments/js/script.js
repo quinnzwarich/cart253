5b732f823e992ff5c26bdf6a6b481a09aa4c42a2
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-05 13:47:37 -0400

    learned about automated movement

--- functions experiments/js/script.js
+++ functions experiments/js/script.js
@@ -1,15 +1,39 @@
 
-let bg = 0;
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
+}
 
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
-  background(bg);
+  background(0);
 
-  if (keyIsDown(65)) {
-    rectMode(CENTER);
-    rect(250, 250, 100, 100);
+  let dx = circle.x - mouseX;
+  let dy = circle.y - mouseY;
+
+  if  (dx < 0) {
+    circle.vx = -circle.speed;
   }
+  else if (dx > 0) {
+    circle.vx = circle.speed;
+  }
+
+  if (dy < 0) {
+    circle.vy = -circle.speed;
+  }
+  else if (dy > 0) {
+    circle.vy = circle.speed;
+  }
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
 }
