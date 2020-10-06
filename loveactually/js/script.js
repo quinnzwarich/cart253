fdc70c5dd54e7b89a6daaeeb0faaaa6d7315dcf2
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-05 22:44:28 -0400

    E3: added more thorough comments

--- loveactually/js/script.js
+++ loveactually/js/script.js
@@ -1,8 +1,14 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+love simulator
+Quinn Zwarich
 
-Here is a description of this template p5 project.
+In this exercise I ask whether it is possible to find certainity through love.
+
+The simulation begins with two circles, one of them being controlled by the user and one
+the other by random movement that makes it seem as if it is blindly searching.
+
+When the circles do meet, they start to orbit each other as if swayed
+but gradually they grow more distant and the background fades to white.
 **************************************************/
 
 let circle1 = {
@@ -11,8 +17,8 @@ let circle1 = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 3
-}
+  speed: 3,
+};
 
 let circle2 = {
   x: 0,
@@ -20,46 +26,39 @@ let circle2 = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 4
-}
+  speed: 4,
+};
 
 let angle = 0;
 let fallingOutRate = 50;
 let bgShade = 0;
 
-let state = `title`; // can be: title, simulation, love, or sadness
+let state = `title`;
 
-// setup()
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // position circles separated from one another
   circle1.x = width / 3;
   circle1.y = height / 2;
-  circle2.x = 2 * width / 3;
+  circle2.x = (2 * width) / 3;
   circle2.y = height / 2;
 }
 
-// draw()
 function draw() {
   background(0);
 
   if (state === `title`) {
     title();
-  }
-  else if (state === `simulation`) {
+  } else if (state === `simulation`) {
     simulation();
-  }
-  else if (state === `neverMetEnding`) {
+  } else if (state === `neverMetEnding`) {
     neverMetEnding();
-  }
-  else if (state === `turnAwayEnding`) {
+  } else if (state === `turnAwayEnding`) {
     turnAwayEnding();
-  }
-  else if (state === `love`) {
+  } else if (state === `love`) {
     love();
-  }
-  else if (state === `loveEnding`) {
+  } else if (state === `loveEnding`) {
     loveEnding();
   }
 }
@@ -71,7 +70,7 @@ function title() {
   textSize(64);
   fill(0);
   textAlign(CENTER, CENTER);
-  text(`love actually`, width/2, height/3);
+  text(`love simulator`, width / 2, height / 3);
   pop();
 }
 
@@ -84,27 +83,18 @@ function simulation() {
 }
 
 function love() {
-  // rotate the circles around each other to symbolize LOVE
   checkFallingOutRate();
 
-  background(bgShade)
-
+  background(bgShade);
   push();
   noStroke();
   textSize(32);
   fill(255);
   textAlign(CENTER, CENTER);
-  text(`is it any more clear now that we've met?`, width/2, 2 * height/3);
+  text(`is it any more clear now that we've met?`, width / 2, (2 * height) / 3);
   pop();
 
-  push();
-  noStroke();
-  textSize(32);
-  fill(bgShade * 1.0312);
-  textAlign(CENTER, CENTER);
-  text(`is it any more clear now that you've left?`, width/2, 4 * height/5);
-  pop();
-
+  // position where the circles met to be the origin around which they rotate
   ellipseMode(CENTER);
   angleMode(DEGREES);
   translate(circle1.x, circle1.y);
@@ -113,6 +103,7 @@ function love() {
   ellipse(-fallingOutRate, 0, circle1.size);
   ellipse(fallingOutRate, 0, circle2.size);
 
+  // make them grow apart
   angle = angle + 0.5;
   fallingOutRate = fallingOutRate + 0.25;
   bgShade = map(fallingOutRate, 50, 1000, 0, 255);
@@ -123,7 +114,7 @@ function neverMetEnding() {
   textSize(32);
   fill(255);
   textAlign(CENTER, CENTER);
-  text(`and maybe you never will...`, width/2, 2 * height/3);
+  text(`and maybe you never will...`, width / 2, (2 * height) / 3);
   pop();
 }
 
@@ -132,7 +123,7 @@ function turnAwayEnding() {
   textSize(32);
   fill(255);
   textAlign(CENTER, CENTER);
-  text(`I know I never will...`, width/2, 2 * height/3);
+  text(`I know I never will...`, width / 2, (2 * height) / 3);
   pop();
 }
 
@@ -141,25 +132,20 @@ function loveEnding() {
 }
 
 function move() {
-
   // allow user to control circle with arrow keys
   if (keyIsDown(LEFT_ARROW)) {
     circle1.vx = -circle1.speed;
-  }
-  else if (keyIsDown(RIGHT_ARROW)) {
+  } else if (keyIsDown(RIGHT_ARROW)) {
     circle1.vx = circle1.speed;
-  }
-  else {
+  } else {
     circle1.vx = 0;
   }
 
   if (keyIsDown(UP_ARROW)) {
     circle1.vy = -circle1.speed;
-  }
-  else if (keyIsDown(DOWN_ARROW)) {
+  } else if (keyIsDown(DOWN_ARROW)) {
     circle1.vy = circle1.speed;
-  }
-  else {
+  } else {
     circle1.vy = 0;
   }
 
@@ -178,30 +164,41 @@ function move() {
 }
 
 function checkOffScreen1() {
-  // check if the circles have gone offscreen
-  if (circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
-      state = `neverMetEnding`;
-    }
+  // check if the non user controlled circle has gone offscreen
+  if (
+    circle2.x < 0 ||
+    circle2.x > width ||
+    circle2.y < 0 ||
+    circle2.y > height
+  ) {
+    state = `neverMetEnding`;
   }
+}
 
 function checkOffScreen2() {
-  // check if the circles have gone offscreen
-  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height) {
-      state = `turnAwayEnding`;
-    }
+  // check if the user controlled circle has gone offscreen
+  if (
+    circle1.x < 0 ||
+    circle1.x > width ||
+    circle1.y < 0 ||
+    circle1.y > height
+  ) {
+    state = `turnAwayEnding`;
   }
+}
 
 function checkOverlap() {
   // check if the circles overlap
   let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
-  if (d < circle1.size/2 + circle2.size/2) {
+  if (d < circle1.size / 2 + circle2.size / 2) {
     state = `love`;
   }
 }
 
 function checkFallingOutRate() {
+  // make sure the screen stays white after having transitioned
   if (fallingOutRate > 1000) {
-    state = 'loveEnding';
+    state = "loveEnding";
   }
 }
 
@@ -218,7 +215,11 @@ function display() {
   textSize(32);
   fill(255);
   textAlign(CENTER, CENTER);
-  text(`you never knew what it was you were looking for`, width/2, 2 * height/3);
+  text(
+    `you never knew what it was you were looking for`,
+    width / 2,
+    (2 * height) / 3
+  );
   pop();
 }
 
