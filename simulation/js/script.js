ae4989fa20dce42db45aecf421913cff6bb8e63b
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-06 03:24:07 -0400

    outlined a prototype for a pinwheel flower pedal

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -7,16 +7,51 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+let angleX = 0;
+let angleY = 0;
 
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(255);
 
+  strokeWeight(5);
+  stroke(0);
+  translate(0, 0, 0);
+  rotateX(angleX);
+  rotateY(angleY);
+  beginShape();
+  vertex(300, 0, 0)
+  vertex(-212, -212, 0);
+  vertex(-400, 0, 0);
+  vertex(300, 0, 0);
+  endShape();
+  push();
+  beginShape();
+  rotateX(PI/2);
+  vertex(300, 0, 0)
+  vertex(-212, -212, 0);
+  vertex(-400, 0, 0);
+  vertex(-212, 212, 0);
+  vertex(300, 0, 0);
+  endShape();
+  pop();
+  push();
+  beginShape();
+  rotateY(PI/2);
+  translate(0, 0, -212);
+  vertex(-212, 0, 0);
+  vertex(212, 0, 0);
+  vertex(0, -212, 0);
+  vertex(-212, 0, 0);
+  endShape();
+  pop();
+
+  angleX = angleX + 0.01;
+  angleY = angleY + 0.05;
 }
