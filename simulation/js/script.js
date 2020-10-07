6452b1656a73d7bc2331fbc3a68b203ab7a7c9c2
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-07 15:52:58 -0400

    attempting to solve problem with flower parameters

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -7,27 +7,32 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-// let pedals = {
-//   x1: 0,
-//   y1: -5,
-//   x2: 0,
-//   y2: -35,
-//   x3: 10,
-//   y3: -15
-// }
-//
-// let stem = {
-//   x1: 0,
-//   y1: 35,
-//   x2: 0,
-//   y2: 65
-// }
+let pedals = {
+  x1: 0,
+  y1: 30,
+  x2: 0,
+  y2: 0,
+  x3: 10,
+  y3: 20
+}
+
+let stem = {
+  x1: 0,
+  y1: 30,
+  x2: 0,
+  y2: 60
+}
+
+let bulb = {
+  x: 0,
+  y: 0
+}
 
 let columns = 0;
 let rows = 0;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1400, 1000);
 
   columns = width / 70;
   rows = height / 100;
@@ -38,35 +43,40 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(255);
-  //drawFlower(0, 0);
+  //drawFlower();
+  //drawFlower();
+  //translate(width/2, height/2);
+  //rotateX(PI/3);
   writeColumnsAndRows();
+  //drawFlower(0, 0);
 
 }
 
 function drawFlower(x, y) {
   translate(30, 30);
-  drawBulb(x, y);
-  drawPedals(x, y, x, y);
-  drawStem(x, y, x, y, x, y);
+  drawPedals(x + pedals.x1, y + pedals.y1, x + pedals.x2, y + pedals.y2, x + pedals.x3, y + pedals.y3);
+  drawBulb(x + bulb.x, y + bulb.y);
+  drawStem(x + stem.x1, y + stem.y1, x + stem.x2, y + stem.y2);
 }
 
-function drawBulb(bulbX = 0, bulbY = 0) {
+function drawBulb() {
   strokeWeight(0.5);
-  ellipse(bulbX, bulbY, 10);
+  ellipse(bulb.x, bulb.y, 10);
 }
 
-function drawPedals(pedalX1 = 0, pedalY1 = 1, pedalX2 = 0, pedalY2 = 0, pedalX3 = 1, pedalY3 = 1) {
+function drawPedals() {
   for (let numPedals = 0; numPedals < 8; numPedals++) {
     rotate(PI/4);
-    triangle(pedalX1, pedalY1 * 30, pedalX2, pedalY2, pedalX3 * 10, pedalY3 * 20);
+    triangle(pedals.x1, pedals.y1, pedals.x2, pedals.y2, pedals.x3, pedals.y3);
   }
 }
 
-function drawStem(stemX1 = 0, stemY1 = 1, stemX2 = 0, stemY2 = 1) {
-  line(stemX1, stemY1 * 30, stemX2, stemY2 * 60);
+function drawStem() {
+  line(stem.x1, stem.y1, stem.x2, stem.y2);
 }
 
 function writeColumnsAndRows() {
+  //translate(-width/2, -height/2);
   for (let k = 0; k < columns; k++) {
     for (let j = 0; j < rows; j++) {
       //rect(k * 70, j * 100, 70, 100);
