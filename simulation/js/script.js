df674221628fcfe62813ec4b4f6a6ff51fb59bd6
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-10 02:55:36 -0400

    user can now move through the noise (flower) field as they hold down the mouse

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -28,18 +28,50 @@ let bulb = {
   y: 0
 }
 
+let user = {
+  vy: 0,
+  ay: 0,
+  maxSpeed: 2,
+  accel: 0.1,
+  move: 0
+
+}
+
 let columns = 0;
 let rows = 0;
-
 let flowerColours = new Array(columns);
 
+// let camX = 0;
+// let camY = 0;
+
 function setup() {
-  createCanvas(1400, 1000, WEBGL);
+let cnv = createCanvas(2800, 2000, WEBGL);
+let cnvX = (windowWidth - width) / 2;
+let cnvY = (windowHeight - height) / 2;
+cnv.position(cnvX, cnvY)
+
   columns = width / 70;
   rows = height / 100;
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(255);
+
+  if (mouseIsPressed) {
+    user.ay = user.accel;
+  }
+  else if (!(mouseIsPressed) && user.vy > 0) {
+    user.ay = -user.accel;
+  }
+  user.vy = user.vy + user.ay;
+  user.vy = constrain(user.vy, 0, user.maxSpeed);
+  user.move = user.move - user.vy;
 
   let xOffset = 0;
-  let yOffset = 0;
+  let yOffset = user.move;
   for (let x = 0; x < columns; x++) {
     flowerColours[x] = new Array(rows);
     for (let y = 0; y < rows - 1; y++)  {
@@ -48,15 +80,15 @@ function setup() {
     }
     xOffset = xOffset + 0.7;
   }
-}
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
-  background(255);
-  rotateX(PI/3);
+  // camX = map(mouseX, 0, width, -200, 200);
+  // camY = map(mouseY, 0, height, -200, 200);
+  // camera(0, 0, (height/2) / tan(PI*30/180), camX, camY, 0, 0, 1, 0);
+  push();
+  //translate(0, 168, 0)
+  rotateX(PI/2.25);
   writeColumnsAndRows();
+  pop();
 }
 
 function drawFlower(x, y, pedalsR, pedalsG, pedalsB) {
@@ -64,6 +96,8 @@ function drawFlower(x, y, pedalsR, pedalsG, pedalsB) {
   translate(30, 30);
   translate(x, y);
   rotateX(-PI/2);
+  strokeWeight(2);
+  stroke(255);
   fill(pedalsR, pedalsG, pedalsB);
   drawPedals();
   drawBulb();
@@ -72,7 +106,6 @@ function drawFlower(x, y, pedalsR, pedalsG, pedalsB) {
 }
 
 function drawBulb() {
-  strokeWeight(0.5);
   ellipse(bulb.x, bulb.y, 10);
 }
 
@@ -87,7 +120,7 @@ function drawStem() {
   line(stem.x1, stem.y1, stem.x2, stem.y2);
 }
 
-function writeColumnsAndRows() {
+function writeColumnsAndRows(x, y) {
   translate(-width/2, -height/2);
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows - 1; y++) {
@@ -95,10 +128,21 @@ function writeColumnsAndRows() {
       push();
       translate(0, 0, -70);
       noStroke();
-      fill(181, 225, 174);
+      fill(211, 255, 204);
       rect(x * 70, y * 100, 70, 100);
       pop();
-      drawFlower(x * 70, y * 100, flowerColours[x], flowerColours[y], flowerColours[x][y]);
+      if (flowerColours[x][y] > 140) {
+        drawFlower(x * 70, y * 100, flowerColours[x], flowerColours[y], flowerColours[x][y]);
+      }
     }
   }
 }
+
+// function renderEnvrionment() {
+//     for (let j = 0; j < rows; j++) {
+//       push();
+//       translate(0, j * -height);
+//       writeColumnsAndRows();
+//       pop();
+//     }
+// }
