cdd0df4fdb8f9328c968d863fae352bc3cb7bbf2
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-11 00:35:09 -0400

    P1: added custom sounds to the project (apologies for forgetting the prefix!)

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -31,18 +31,15 @@ let bulb = {
 let user = {
   vy: 0,
   ay: 0,
-  maxSpeed: 2,
-  accel: 0.1,
+  maxSpeed: 0.1,
+  accel: 0.01,
   move: 0
-
 }
 
 let columns = 0;
 let rows = 0;
 let flowerColours = new Array(columns);
-
-// let camX = 0;
-// let camY = 0;
+let polySynth;
 
 function setup() {
 let cnv = createCanvas(2800, 2000, WEBGL);
@@ -50,8 +47,10 @@ let cnvX = (windowWidth - width) / 2;
 let cnvY = (windowHeight - height) / 2;
 cnv.position(cnvX, cnvY)
 
-  columns = width / 70;
-  rows = height / 100;
+polySynth = new p5.PolySynth();
+
+columns = width / 70;
+rows = height / 100;
 }
 
 // draw()
@@ -60,35 +59,14 @@ cnv.position(cnvX, cnvY)
 function draw() {
   background(255);
 
-  if (mouseIsPressed) {
-    user.ay = user.accel;
-  }
-  else if (!(mouseIsPressed) && user.vy > 0) {
-    user.ay = -user.accel;
-  }
-  user.vy = user.vy + user.ay;
-  user.vy = constrain(user.vy, 0, user.maxSpeed);
-  user.move = user.move - user.vy;
+  movement();
+  writeArray();
 
-  let xOffset = 0;
-  let yOffset = user.move;
-  for (let x = 0; x < columns; x++) {
-    flowerColours[x] = new Array(rows);
-    for (let y = 0; y < rows - 1; y++)  {
-      flowerColours[x][y] = map(noise(xOffset,yOffset), 0, 1, 0, 255);
-      yOffset = yOffset + 1;
-    }
-    xOffset = xOffset + 0.7;
-  }
-
-  // camX = map(mouseX, 0, width, -200, 200);
-  // camY = map(mouseY, 0, height, -200, 200);
-  // camera(0, 0, (height/2) / tan(PI*30/180), camX, camY, 0, 0, 1, 0);
   push();
-  //translate(0, 168, 0)
   rotateX(PI/2.25);
   writeColumnsAndRows();
   pop();
+
 }
 
 function drawFlower(x, y, pedalsR, pedalsG, pedalsB) {
@@ -120,6 +98,52 @@ function drawStem() {
   line(stem.x1, stem.y1, stem.x2, stem.y2);
 }
 
+function writeArray() {
+  // two dimensional array stores values for noise space
+  // offsets progress through these values
+  let xOffset = 0;
+  let yOffset = user.move;
+  for (let x = 0; x < columns; x++) {
+    flowerColours[x] = new Array(rows);
+    for (let y = 0; y < rows - 1; y++)  {
+      flowerColours[x][y] = map(noise(xOffset,yOffset), 0, 1, 0, 255);
+      yOffset = yOffset + 1;
+      // play notes according to d maj pentatonic and f# min pentatoni
+      if (flowerColours[y] > 155 && flowerColours[x][y] <= 165) {
+        polySynth.play('D3', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 165 && flowerColours[x][y] <= 175) {
+        polySynth.play('E3', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 175 && flowerColours[x][y] <= 185) {
+        polySynth.play('F#3', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 185 && flowerColours[x][y] <= 195) {
+        polySynth.play('A3', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 195 && flowerColours[x][y] <= 205) {
+        polySynth.play('B3', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 205 && flowerColours[x][y] <= 215) {
+        polySynth.play('F#4', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 215 && flowerColours[x][y] <= 225) {
+        polySynth.play('A4', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 225 && flowerColours[x][y] <= 235) {
+        polySynth.play('B4', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 235 && flowerColours[x][y] <= 245) {
+        polySynth.play('C#5', 0.1, 0, 1);
+      }
+      else if (flowerColours[y] > 245 && flowerColours[x][y] <= 255) {
+        polySynth.play('E5', 0.1, 0, 1);
+      }
+    }
+    xOffset = xOffset + 0.7;
+  }
+}
+
 function writeColumnsAndRows(x, y) {
   translate(-width/2, -height/2);
   for (let x = 0; x < columns; x++) {
@@ -131,18 +155,23 @@ function writeColumnsAndRows(x, y) {
       fill(211, 255, 204);
       rect(x * 70, y * 100, 70, 100);
       pop();
-      if (flowerColours[x][y] > 140) {
+      //choose when to draw a flower
+      if (flowerColours[x][y] > 155) {
         drawFlower(x * 70, y * 100, flowerColours[x], flowerColours[y], flowerColours[x][y]);
       }
     }
   }
 }
 
-// function renderEnvrionment() {
-//     for (let j = 0; j < rows; j++) {
-//       push();
-//       translate(0, j * -height);
-//       writeColumnsAndRows();
-//       pop();
-//     }
-// }
+function movement() {
+  // user moves forward through pressing the mouse
+  if (mouseIsPressed) {
+    user.ay = user.accel;
+  }
+  else if (!(mouseIsPressed) && user.vy > 0) {
+    user.ay = -user.accel;
+  }
+  user.vy = user.vy + user.ay;
+  user.vy = constrain(user.vy, 0, user.maxSpeed);
+  user.move = user.move - user.vy;
+}
