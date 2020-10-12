32030019a6d40a40897d8ed3a2e59de3065bbd1d
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-12 19:10:47 -0400

    P1: made it so that flowers have not only colour and position values generated with noise but as well angle values

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -9,86 +9,109 @@ Here is a description of this template p5 proyect.
 
 let pedals = {
   x1: 0,
-  y1: 30,
+  y1: 15,
   x2: 0,
   y2: 0,
-  x3: 10,
-  y3: 20
+  x3: 5,
+  y3: 10
 }
 
 let stem = {
   x1: 0,
-  y1: 30,
+  y1: 0,
   x2: 0,
   y2: 60
 }
 
 let bulb = {
   x: 0,
-  y: 0
+  y: 0,
+  z: 1
 }
 
 let user = {
   vy: 0,
   ay: 0,
-  maxSpeed: 0.1,
-  accel: 0.01,
+  maxSpeed: 2,
+  accel: 0.1,
   move: 0
 }
 
+let cnv;
+let font;
+
 let columns = 0;
 let rows = 0;
+let flowerAngles = new Array(columns);
 let flowerColours = new Array(columns);
-let polySynth;
+
+let state = `title`;
+
+function preload() {
+  font = loadFont('assets/fonts/england.ttf')
+}
 
 function setup() {
-let cnv = createCanvas(2800, 2000, WEBGL);
-let cnvX = (windowWidth - width) / 2;
-let cnvY = (windowHeight - height) / 2;
-cnv.position(cnvX, cnvY)
+  let cnv = createCanvas(1000, 1000, WEBGL);
+  let cnvX = (windowWidth - width) / 2;
+  let cnvY = (windowHeight - height) / 2;
+  cnv.position(cnvX, cnvY)
 
-polySynth = new p5.PolySynth();
-
-columns = width / 70;
-rows = height / 100;
+  columns = width / 10;
+  rows = height / 10;
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(255);
+  if (state === `title`) {
+    title();
+  }
+}
 
-  movement();
+function title() {
+  background(211, 204, 255);
   writeArray();
 
   push();
-  rotateX(PI/2.25);
-  writeColumnsAndRows();
+  fill(255);
+  translate(0, 250, 250);
+  textFont(font);
+  textSize(75);
+  textAlign(CENTER, CENTER);
+  text(`Field of Artificial Flowers`, 0, 0);
   pop();
 
-}
-
-function drawFlower(x, y, pedalsR, pedalsG, pedalsB) {
   push();
-  translate(30, 30);
-  translate(x, y);
-  rotateX(-PI/2);
-  strokeWeight(2);
-  stroke(255);
-  fill(pedalsR, pedalsG, pedalsB);
-  drawPedals();
-  drawBulb();
-  drawStem();
+  rotateX(PI/3);
+  drawGrass();
+  pop();
+  push();
+  rotateX(PI/3);
+  drawFlowers(user.move);
   pop();
 }
 
-function drawBulb() {
-  ellipse(bulb.x, bulb.y, 10);
+function drawFlower(x, y, z, pedalsR, pedalsG, pedalsB, v) {
+  push();
+  translate(0, 0, 250);
+  translate(x, y, z);
+  rotateX(-PI/2);
+  strokeWeight(0.5);
+  stroke(161, 205, 154);
+  drawStem();
+  fill(pedalsR, pedalsG, pedalsB);
+  stroke(pedalsR, pedalsG, pedalsB);
+  rotateX(PI);
+  rotateY(2 * PI);
+  drawPedals(v);
+  pop();
 }
 
-function drawPedals() {
+function drawPedals(v) {
   for (let numPedals = 0; numPedals < 8; numPedals++) {
+    rotateY(v);
     rotate(PI/4);
     triangle(pedals.x1, pedals.y1, pedals.x2, pedals.y2, pedals.x3, pedals.y3);
   }
@@ -102,76 +125,55 @@ function writeArray() {
   // two dimensional array stores values for noise space
   // offsets progress through these values
   let xOffset = 0;
-  let yOffset = user.move;
+  let yOffset = 0;
   for (let x = 0; x < columns; x++) {
     flowerColours[x] = new Array(rows);
+    flowerAngles[x] = new Array(rows);
     for (let y = 0; y < rows - 1; y++)  {
       flowerColours[x][y] = map(noise(xOffset,yOffset), 0, 1, 0, 255);
-      yOffset = yOffset + 1;
-      // play notes according to d maj pentatonic and f# min pentatoni
-      if (flowerColours[y] > 155 && flowerColours[x][y] <= 165) {
-        polySynth.play('D3', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 165 && flowerColours[x][y] <= 175) {
-        polySynth.play('E3', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 175 && flowerColours[x][y] <= 185) {
-        polySynth.play('F#3', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 185 && flowerColours[x][y] <= 195) {
-        polySynth.play('A3', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 195 && flowerColours[x][y] <= 205) {
-        polySynth.play('B3', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 205 && flowerColours[x][y] <= 215) {
-        polySynth.play('F#4', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 215 && flowerColours[x][y] <= 225) {
-        polySynth.play('A4', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 225 && flowerColours[x][y] <= 235) {
-        polySynth.play('B4', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 235 && flowerColours[x][y] <= 245) {
-        polySynth.play('C#5', 0.1, 0, 1);
-      }
-      else if (flowerColours[y] > 245 && flowerColours[x][y] <= 255) {
-        polySynth.play('E5', 0.1, 0, 1);
-      }
+      flowerAngles[x][y] = map(noise(xOffset, yOffset), 0, 1, PI, 2 * PI);
+      yOffset = yOffset + 2.5;
     }
-    xOffset = xOffset + 0.7;
+    xOffset = xOffset + 2.5;
   }
 }
 
-function writeColumnsAndRows(x, y) {
+function drawGrass() {
   translate(-width/2, -height/2);
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows - 1; y++) {
       // draw grass
       push();
-      translate(0, 0, -70);
+      translate(0, 0, 150);
       noStroke();
       fill(211, 255, 204);
-      rect(x * 70, y * 100, 70, 100);
+      rect(x * 10, y * 10, 10, 10);
       pop();
-      //choose when to draw a flower
+    }
+  }
+}
+
+function drawFlowers(z) {
+  translate(-width/2, -height/2);
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows - 1; y++) {
+      // draw patches of flowers using noise
       if (flowerColours[x][y] > 155) {
-        drawFlower(x * 70, y * 100, flowerColours[x], flowerColours[y], flowerColours[x][y]);
+        drawFlower(x * 10, y * 10, z, 255, flowerColours[x][y], flowerColours[x][y], flowerAngles[x][y]);
       }
     }
   }
 }
 
-function movement() {
-  // user moves forward through pressing the mouse
-  if (mouseIsPressed) {
-    user.ay = user.accel;
-  }
-  else if (!(mouseIsPressed) && user.vy > 0) {
-    user.ay = -user.accel;
-  }
+
+
+function mousePressed() {
+  user.ay = user.accel;
   user.vy = user.vy + user.ay;
   user.vy = constrain(user.vy, 0, user.maxSpeed);
-  user.move = user.move - user.vy;
+  user.move = user.move + user.vy;
 }
+
+  // else if (!(mouseIsPressed) && user.vy > 0) {
+  //   user.ay = -user.accel;
+  // }
