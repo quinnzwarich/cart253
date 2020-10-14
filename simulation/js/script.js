17d221e76bc4b9aedfd78c758708ac120ad1140a
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-13 21:04:01 -0400

    P1: implented flower levitation with acceleration

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -24,21 +24,22 @@ let stem = {
 }
 
 let levitate = {
-  vy: 0,
-  ay: 0,
-  maxSpeed: 2,
-  accel: 0.1,
+  z: 0,
+  maxSpeed: 5,
+  accel: 0.25,
 }
 
 let cnv;
 let font;
 
-let r = 0;
+let frames;
 
 let columns = 0;
 let rows = 0;
+let scale = 50;
 
 let movements = new Array(columns);
+let zCoordinates = new Array(columns);
 let flowers = new Array(columns);
 let flowerAngles = new Array(columns);
 let flowerColours = new Array(columns);
@@ -55,8 +56,12 @@ function setup() {
   let cnvY = (windowHeight - height) / 2;
   cnv.position(cnvX, cnvY)
 
-  columns = width / 10;
-  rows = height / 10;
+  columns = width / scale;
+  rows = height / scale;
+
+  frames = createP('');
+
+  writeArray();
 }
 
 // draw()
@@ -65,6 +70,9 @@ function setup() {
 function draw() {
   if (state === `title`) {
     title();
+  }
+  if (state === `lovesMe`) {
+    lovesMe();
   }
 }
 
@@ -84,11 +92,11 @@ function draw() {
 
 function drawFlower(x, y, z, pedalsR, pedalsG, pedalsB, angle) {
     push();
-    translate(0, 0, 250);
+    translate(250, 650, 250);
     translate(x, y, z);
     rotateX(-PI/2);
     strokeWeight(0.5);
-    stroke(161, 205, 154);
+    stroke(211, 255, 204);
     drawStem();
     fill(pedalsR, pedalsG, pedalsB);
     stroke(pedalsR, pedalsG, pedalsB);
@@ -111,14 +119,13 @@ function drawStem() {
 }
 
 function title() {
-  background(211, 204, 255);
-  writeArray();
+  background(0, 127, 255);
 
   push();
   fill(255);
-  translate(0, 250, 250);
+  translate(0, -275, 250);
   textFont(font);
-  textSize(75);
+  textSize(70);
   textAlign(CENTER, CENTER);
   text(`Field of Artificial Flowers`, 0, 0);
   pop();
@@ -131,24 +138,9 @@ function title() {
   rotateX(PI/3);
   drawFlowers();
   pop();
-}
 
-// function drawFlower(x, y, z, pedalsR, pedalsG, pedalsB, vector) {
-//   push();
-//   translate(0, 0, 250);
-//   translate(x, y, z);
-//   rotateX(-PI/2);
-//   strokeWeight(0.5);
-//   stroke(161, 205, 154);
-//   drawStem();
-//   fill(pedalsR, pedalsG, pedalsB);
-//   stroke(pedalsR, pedalsG, pedalsB);
-//   rotateX(PI);
-//   rotateY(2 * PI);
-//   drawPedals(v);
-//   pop();
-// }
-//
+  frames.html(floor(frameRate()));
+}
 
 function writeArray() {
   // two dimensional array stores values for noise space
@@ -160,59 +152,77 @@ function writeArray() {
     flowerAngles[x] = new Array(rows);
     flowers[x] = new Array(rows);
     movements[x] = new Array(rows);
+    zCoordinates[x] = new Array(rows);
     for (let y = 0; y < rows - 1; y++)  {
       flowerColours[x][y] = map(noise(xOffset,yOffset), 0, 1, 0, 255);
       flowerAngles[x][y] = map(noise(xOffset, yOffset), 0, 1, PI, 2 * PI);
-      yOffset = yOffset + 2.5;
+      yOffset = yOffset + 1.5;
+
+      movements[x][y] = 0;
+      zCoordinates[x][y] = 0;
     }
-    xOffset = xOffset + 2.5;
+    xOffset = xOffset + 1.5;
   }
 }
 
 function drawGrass() {
   translate(-width/2, -height/2);
-  for (let x = 0; x < columns; x++) {
-    for (let y = 0; y < rows - 1; y++) {
-      // draw grass
-      push();
-      translate(0, 0, 150);
-      noStroke();
-      fill(211, 255, 204);
-      rect(x * 10, y * 10, 10, 10);
-      pop();
-    }
-  }
+  // draw grass
+  push();
+  translate(0, 0, 150);
+  noStroke();
+  fill(186, 230, 179);
+  rect(-width / 4, height / 6, width * 2, height);
+  pop();
 }
 
 function drawFlowers() {
-  movement();
-
   randomSeed = (99);
   translate(-width/2, -height/2);
+
+  if (mouseIsPressed) {
+    movements[randomPick.x][randomPick.y] = movements[randomPick.x][randomPick.y] + levitate.accel;
+    movements[randomPick.x][randomPick.y] = constrain(movements[randomPick.x][randomPick.y], 0, levitate.maxSpeed);
+    zCoordinates[randomPick.x][randomPick.y] = zCoordinates[randomPick.x][randomPick.y] + movements[randomPick.x][randomPick.y];
+    console.log(zCoordinates[randomPick.x][randomPick.y]);
+  }
+
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows - 1; y++) {
       // draw patches of flowers using noise
       if (flowerColours[x][y] > 155) {
-        flowers.push(drawFlower(x * 10, y * 10, 0, 255, flowerColours[x][y], flowerColours[x][y], flowerAngles[x][y]));
-        movements.push(0);
-    }
-  }
-      if (mouseIsPressed) {
-        r = int(random(flowers[0][0], flowers[columns - 1][rows - 1]));
-        flowers[r][r] = drawFlower(r * 10, r * 10, z, 255, flowerColours[r][r], flowerColours[r][r], flowerAngles[r][r])
+        let rValue = flowerColours[x];
+        if(randomPick!=undefined && x === randomPick.x && y ===randomPick.y ) {
+          rValue = flowerColours[y];
+
+        }
+        drawFlower(x * scale/2, y * scale/2, zCoordinates[x][y], rValue, flowerColours[y], flowerColours[x][y], flowerAngles[x][y]);
       }
     }
   }
-
-function movement(z = 0, ay = 0, vy = 0) {
-  ay = levitate.accel;
-  vy = vy + ay;
-  vy = constrain(vy, 0, levitate.maxSpeed);
-  z = z + vy;
 }
 
+// function movement(velocity) {
+//   velocity = velocity + levitate.accel;
+//   velocity = constrain(velocity, 0, levitate.maxSpeed);
+//   zCoordinates[x][y] = zCoordinates[x][y] + velocity;
+// }
+
+let randomPick;
+let pickedFlowerZ = 0;
+
 function mousePressed() {
-  movement();
+  randomPick = {
+    x: floor(random(columns)),
+    y: floor(random(rows))
+  };
+
+  while(flowerColours[randomPick.x][randomPick.y] <= 155) {
+    randomPick = {
+      x: floor(random(columns)),
+      y: floor(random(rows))
+    };
+  }
 }
 
   // else if (!(mouseIsPressed) && user.vy > 0) {
