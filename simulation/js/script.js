9c8a5637f212b38aeef177d447a2f4c83189be0a
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-13 23:48:13 -0400

    P1: revised and added more thorough comments

--- simulation/js/script.js
+++ simulation/js/script.js
@@ -1,10 +1,14 @@
 "use strict";
 
 /**************************************************
-Template p5 proyect
-Pippin Barr
+Field of Artificial Flowers
+Quinn Zwarich
 
-Here is a description of this template p5 proyect.
+This simulation generates a field of flowers using perlin noise.
+The idea behind the interaction is that it becomes an unending game of
+"loves me... loves me not..." that the user play through uprooting flowers
+(as opposed to delicately plucking pedals). In doing so, they also generate notes
+from a minor pentatonic scale.
 **************************************************/
 
 let pedals = {
@@ -13,21 +17,21 @@ let pedals = {
   x2: 0,
   y2: 0,
   x3: 5,
-  y3: 10
-}
+  y3: 10,
+};
 
 let stem = {
   x1: 0,
   y1: 0,
   x2: 0,
-  y2: 60
-}
+  y2: 60,
+};
 
 let levitate = {
   z: 0,
   maxSpeed: 5,
   accel: 0.25,
-}
+};
 
 let cnv;
 let font;
@@ -52,77 +56,60 @@ let flowerColours = new Array(columns);
 let state = `title`;
 
 function preload() {
-  font = loadFont('assets/fonts/england.ttf');
+  font = loadFont("assets/fonts/england.ttf");
   note1 = loadSound(`assets/sounds/Fsharp3.wav`);
   note2 = loadSound(`assets/sounds/A3.wav`);
   note3 = loadSound(`assets/sounds/B3.wav`);
   note4 = loadSound(`assets/sounds/Csharp4.wav`);
-  note1 = loadSound(`assets/sounds/E4.wav`);
+  note5 = loadSound(`assets/sounds/E4.wav`);
 }
 
 function setup() {
+  // aligns the canvas so that it displays from the center of the window
+  // taken from the wiki
   let cnv = createCanvas(1000, 1000, WEBGL);
   let cnvX = (windowWidth - width) / 2;
   let cnvY = (windowHeight - height) / 2;
-  cnv.position(cnvX, cnvY)
+  cnv.position(cnvX, cnvY);
 
   columns = width / scale;
   rows = height / scale;
 
-  frames = createP('');
-
   writeArray();
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
   if (state === `title`) {
     title();
-  }
-  else if (state === `lovesMe`) {
+  } else if (state === `lovesMe`) {
     lovesMe();
-  }
-  else if (state === `lovesMeNot`) {
+  } else if (state === `lovesMeNot`) {
     lovesMeNot();
   }
 }
 
-// class Flower {
-//   constructor() {
-//     this.pedalX1 = 0;
-//     this.pedalY1 = 15;
-//     this.pedalX2 = 0;
-//     this.pedalY2 = 0;
-//     this.pedalX3 = 5;
-//     this.pedalY3 = 10;
-//     this.stemX1 = 0;
-//     this.stemY1 = 0;
-//     this.stemX2 = 0;
-//     this.stemY2 = 60;
-//   }
-
+// the flower is drawn by rotating a series of triangles along the origin of a line,
+// various transformations are applied to have the design register as being 3 dimensional
 function drawFlower(x, y, z, pedalsR, pedalsG, pedalsB, angle) {
-    push();
-    translate(250, 650, 250);
-    translate(x, y, z);
-    rotateX(-PI/2);
-    strokeWeight(0.5);
-    stroke(211, 255, 204);
-    drawStem();
-    fill(pedalsR, pedalsG, pedalsB);
-    stroke(pedalsR, pedalsG, pedalsB);
-    rotateX(PI);
-    rotateY(2 * PI);
-    drawPedals(angle);
-    pop();
-  }
+  push();
+  translate(250, 650, 250);
+  translate(x, y, z);
+  rotateX(-PI / 2);
+  strokeWeight(0.5);
+  stroke(211, 255, 204);
+  drawStem();
+  fill(pedalsR, pedalsG, pedalsB);
+  stroke(pedalsR, pedalsG, pedalsB);
+  rotateX(PI);
+  rotateY(2 * PI);
+  drawPedals(angle);
+  pop();
+}
 
 function drawPedals(tempAngle) {
   for (let numPedals = 0; numPedals < 8; numPedals++) {
     rotateY(tempAngle);
-    rotate(PI/4);
+    rotate(PI / 4);
     triangle(pedals.x1, pedals.y1, pedals.x2, pedals.y2, pedals.x3, pedals.y3);
   }
 }
@@ -132,6 +119,7 @@ function drawStem() {
 }
 
 function title() {
+  //if the background were drawn in setup, you would see multiples of the flowers as they grew !
   background(0, 127, 255);
 
   push();
@@ -144,11 +132,11 @@ function title() {
   pop();
 
   push();
-  rotateX(PI/3);
+  rotateX(PI / 3);
   drawGrass();
   pop();
   push();
-  rotateX(PI/3);
+  rotateX(PI / 3);
   drawFlowers();
   pop();
 }
@@ -158,7 +146,7 @@ function lovesMe() {
 
   push();
   fill(255);
-  rotateX(PI/3);
+  rotateX(PI / 3);
   translate(-87.5, 600, 250);
   textFont(font);
   textSize(35);
@@ -166,11 +154,11 @@ function lovesMe() {
   pop();
 
   push();
-  rotateX(PI/3);
+  rotateX(PI / 3);
   drawGrass();
   pop();
   push();
-  rotateX(PI/3);
+  rotateX(PI / 3);
   drawFlowers();
   pop();
 }
@@ -180,7 +168,7 @@ function lovesMeNot() {
 
   push();
   fill(255);
-  rotateX(PI/3);
+  rotateX(PI / 3);
   translate(-87.5, 600, 250);
   textFont(font);
   textSize(35);
@@ -188,18 +176,18 @@ function lovesMeNot() {
   pop();
 
   push();
-  rotateX(PI/3);
+  rotateX(PI / 3);
   drawGrass();
   pop();
   push();
-  rotateX(PI/3);
+  rotateX(PI / 3);
   drawFlowers();
   pop();
 }
 
 function writeArray() {
-  // two dimensional array stores values for noise space
-  // offsets progress through these values
+  // two dimensional arrays store values from noise space
+  // I learned concepts involving arrays from the coding train
   let xOffset = 0;
   let yOffset = 0;
   for (let x = 0; x < columns; x++) {
@@ -208,8 +196,8 @@ function writeArray() {
     flowers[x] = new Array(rows);
     movements[x] = new Array(rows);
     zCoordinates[x] = new Array(rows);
-    for (let y = 0; y < rows - 1; y++)  {
-      flowerColours[x][y] = map(noise(xOffset,yOffset), 0, 1, 0, 255);
+    for (let y = 0; y < rows - 1; y++) {
+      flowerColours[x][y] = map(noise(xOffset, yOffset), 0, 1, 0, 255);
       flowerAngles[x][y] = map(noise(xOffset, yOffset), 0, 1, PI, 2 * PI);
       yOffset = yOffset + 1.75;
 
@@ -221,7 +209,7 @@ function writeArray() {
 }
 
 function drawGrass() {
-  translate(-width/2, -height/2);
+  translate(-width / 2, -height / 2);
   // draw grass
   push();
   translate(0, 0, 150);
@@ -232,79 +220,100 @@ function drawGrass() {
 }
 
 function drawFlowers() {
-  randomSeed = (99);
-  translate(-width/2, -height/2);
+  randomSeed = 99;
+  translate(-width / 2, -height / 2);
 
   if (mouseIsPressed) {
-    movements[randomPick.x][randomPick.y] = movements[randomPick.x][randomPick.y] + levitate.accel;
-    movements[randomPick.x][randomPick.y] = constrain(movements[randomPick.x][randomPick.y], 0, levitate.maxSpeed);
-    zCoordinates[randomPick.x][randomPick.y] = zCoordinates[randomPick.x][randomPick.y] + movements[randomPick.x][randomPick.y];
-    console.log(zCoordinates[randomPick.x][randomPick.y]);
+    movements[randomPick.x][randomPick.y] =
+      movements[randomPick.x][randomPick.y] + levitate.accel;
+    movements[randomPick.x][randomPick.y] = constrain(
+      movements[randomPick.x][randomPick.y],
+      0,
+      levitate.maxSpeed
+    );
+    zCoordinates[randomPick.x][randomPick.y] =
+      zCoordinates[randomPick.x][randomPick.y] +
+      movements[randomPick.x][randomPick.y];
   }
-
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows - 1; y++) {
-      // draw patches of flowers using noise
+      // draws patches of flowers according to noise values
+      // if the flower is being levitated change its colour
       if (flowerColours[x][y] > 155) {
         let rValue = flowerColours[x];
-        if(randomPick!=undefined && x === randomPick.x && y ===randomPick.y ) {
+        if (
+          randomPick != undefined &&
+          x === randomPick.x &&
+          y === randomPick.y
+        ) {
           rValue = flowerColours[y];
-
         }
-        drawFlower(x * scale/2, y * scale/2, zCoordinates[x][y], rValue, flowerColours[y], flowerColours[x][y], flowerAngles[x][y]);
+        drawFlower(
+          (x * scale) / 2,
+          (y * scale) / 2,
+          zCoordinates[x][y],
+          rValue,
+          flowerColours[y],
+          flowerColours[x][y],
+          flowerAngles[x][y]
+        );
       }
     }
   }
 }
 
-// function movement(velocity) {
-//   velocity = velocity + levitate.accel;
-//   velocity = constrain(velocity, 0, levitate.maxSpeed);
-//   zCoordinates[x][y] = zCoordinates[x][y] + velocity;
-// }
-
+// I received generous amounts of help from sam on this picking algorithm
+// in order to find the locations of flowers, a while loop searches for random values
+// until it finds one that is above 155 (the metric I used to plant flowers according to perlin noise)
 let randomPick;
-
 function mousePressed() {
   randomPick = {
     x: floor(random(columns)),
-    y: floor(random(rows))
+    y: floor(random(rows)),
   };
 
-  while(flowerColours[randomPick.x][randomPick.y] < 155) {
+  while (flowerColours[randomPick.x][randomPick.y] < 155) {
     randomPick = {
       x: floor(random(columns)),
-      y: floor(random(rows))
+      y: floor(random(rows)),
     };
   }
+
   if (flowerColours[randomPick.x][randomPick.y] > 155) {
     if (state === `title`) {
       state = `lovesMe`;
-    }
-    else if (state === `lovesMe`) {
+    } else if (state === `lovesMe`) {
       state = `lovesMeNot`;
-    }
-    else if (state === `lovesMeNot`) {
+    } else if (state === `lovesMeNot`) {
       state = `lovesMe`;
     }
   }
-  if (flowerColours[randomPick.x][randomPick.y] > 155 && flowerColours[randomPick.x][randomPick.y] <= 175) {
+
+  // play notes according to noise values, specifically those which have informed the location of a flower
+  if (
+    flowerColours[randomPick.x][randomPick.y] > 155 &&
+    flowerColours[randomPick.x][randomPick.y] <= 160
+  ) {
     note1.play();
-  }
-  else if (flowerColours[randomPick.x][randomPick.y] > 175 && flowerColours[randomPick.x][randomPick.y] <= 195) {
+  } else if (
+    flowerColours[randomPick.x][randomPick.y] > 160 &&
+    flowerColours[randomPick.x][randomPick.y] <= 165
+  ) {
     note2.play();
-  }
-  else if (flowerColours[randomPick.x][randomPick.y] > 195 && flowerColours[randomPick.x][randomPick.y] <= 215) {
+  } else if (
+    flowerColours[randomPick.x][randomPick.y] > 165 &&
+    flowerColours[randomPick.x][randomPick.y] <= 170
+  ) {
     note3.play();
-  }
-  else if (flowerColours[randomPick.x][randomPick.y] > 215 && flowerColours[randomPick.x][randomPick.y] <= 235) {
+  } else if (
+    flowerColours[randomPick.x][randomPick.y] > 170 &&
+    flowerColours[randomPick.x][randomPick.y] <= 175
+  ) {
     note4.play();
-  }
-  else if (flowerColours[randomPick.x][randomPick.y] > 235 && flowerColours[randomPick.x][randomPick.y] <= 255) {
+  } else if (
+    flowerColours[randomPick.x][randomPick.y] > 175 &&
+    flowerColours[randomPick.x][randomPick.y] <= 255
+  ) {
     note5.play();
   }
 }
-
-  // else if (!(mouseIsPressed) && user.vy > 0) {
-  //   user.ay = -user.accel;
-  // }
