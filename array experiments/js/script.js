8811c1730fe7bd84b95294abdcef59724ff6c460
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-19 02:15:02 -0400

    E4: prepped a "boo" function that you cannot see until you move your cursor close enough to it

--- array experiments/js/script.js
+++ array experiments/js/script.js
@@ -7,50 +7,73 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-let circle = {
-  x: 0,
-  y: 0,
-  size: 100,
-  trail: [],
-  trailSize: 25.5
-};
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-  createCanvas(500, 500);
+
+let feistyBoo;
+let shyBoo;
+
+let boo1;
+
+function preload() {
+  feistyBoo = loadImage(`assets/images/boo.png`);
+  shyBoo = loadImage(`assets/images/shyboo.png`);
 }
 
-// draw()
-//
-// Description of draw() goes here.
+function setup() {
+  createCanvas(1200, 1200);
+  boo1 = createBoo(600, 600);
+}
+
+function createBoo(x, y) {
+  let boo = {
+    x: x,
+    y: y,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    tx: 0,
+    ty: 10,
+    size: 415,
+    trail: [],
+    trailSize: 5
+  };
+  return boo;
+}
+
+function moveBoo(boo) {
+  boo.tx = boo.tx + 0.025;
+  boo.ty = boo.ty + 0.025;
+
+  let noiseX = noise(boo.tx);
+  let noiseY = noise(boo.ty);
+
+  boo.vx = map(noiseX, 0, 1, -boo.speed, boo.speed);
+  boo.vy = map(noiseY, 0, 1, -boo.speed, boo.speed);
+
+  boo.x = boo.x + boo.vx;
+  boo.y = boo.y + boo.vy;
+
+  let previousVX = boo.vx;
+  let previousVY = boo.vy;
+  let dx = boo.x - mouseX;
+  let dy = boo.y - mouseY;
+
+  
+}
+
+function displayBoo(boo) {
+  let d = dist(mouseX, mouseY, boo.x, boo.y);
+  let shade = map(d, width/2, 0, 0, 255);
+  push();
+  imageMode(CENTER);
+  tint(shade);
+  image(shyBoo, boo.x, boo.y);
+  pop();
+}
+
 function draw() {
   background(0);
 
-  circle.x = mouseX;
-  circle.y = mouseY;
-
-  for (let i = 0; i < circle.trail.length; i++) {
-    let position = circle.trail[i];
-    let shade = map(i, 0, circle.trail.length, 0, 255);
-    noStroke();
-    fill(shade, shade, shade, shade);
-    ellipse(position.x, position.y, circle.size);
-  }
-
-  push();
-  ellipse(circle.x, circle.y, circle.size);
-  pop();
-
-
-  let newTrailPosition = {
-    x: circle.x,
-    y: circle.y
-  };
-  circle.trail.push(newTrailPosition);
-
-  if (circle.trail.length > circle.trailSize) {
-    circle.trail.shift();
-  }
+  moveBoo(boo1);
+  displayBoo(boo1);
 }
