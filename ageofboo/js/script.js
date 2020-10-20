ac31c0136660e5274d4523e45cfede56c16246af
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-10-20 16:19:12 -0400

    E4: actually did all of those things from prior commit

--- ageofboo/js/script.js
+++ ageofboo/js/script.js
@@ -1,10 +1,13 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+ageofboo
+Quinn Zwarich
 
-Here is a description of this template p5 project.
+In this simulation, you embody a boo which must shepard their other more
+nervous brethren to stay within the bounds of the canvas, if one floats away
+and gets lost it's on you!
+You asked for something spooky for this assignment, right?
 **************************************************/
 
 let user = {
@@ -12,10 +15,10 @@ let user = {
   y: 0,
   xPositions: [],
   maxPositions: 2,
-  direction: [`left`, `right`]
+  direction: [`left`, `right`],
 };
 
-let spookyMusic
+let spookyMusic;
 let booLaugh;
 let shyBoo;
 let feistyBoo;
@@ -38,10 +41,19 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < 5; i++) {
-    booWho[i] = createBoo(random(width/4, 3 * width/4), random(height/4, 3 * height/4), random(0, 10), random (0,10), random(0, 0.1));
+    // all parameters are randomly determined
+    booWho[i] = createBoo(
+      random(width / 4, (3 * width) / 4),
+      random(height / 4, (3 * height) / 4),
+      random(0, 10),
+      random(0, 10),
+      random(0, 0.1)
+    );
   }
 }
 
+// in addition to individual coordinates,
+// each boo has separate offsets as well as a unique increment through the noise space
 function createBoo(x, y, tx, ty, increment) {
   let boo = {
     x: x,
@@ -54,7 +66,7 @@ function createBoo(x, y, tx, ty, increment) {
     increment: increment,
     size: 415,
     trail: [],
-    trailSize: 5
+    trailSize: 5,
   };
   return boo;
 }
@@ -63,6 +75,7 @@ function moveBooAndCheckOffscreen(boo) {
   boo.tx = boo.tx + boo.increment;
   boo.ty = boo.ty + boo.increment;
 
+  // increase their speed so that the simulation becomes more frantic as it progresses
   boo.speed = boo.speed + 0.05;
   boo.speed = constrain(boo.speed, 0, 20);
 
@@ -71,30 +84,33 @@ function moveBooAndCheckOffscreen(boo) {
 
   let dx = boo.x - mouseX;
   let dy = boo.y - mouseY;
-  let d2 = dist(mouseX, mouseY, boo.x, boo.y);
+  let d1 = dist(mouseX, mouseY, boo.x, boo.y);
 
-  if (d2 < boo.size) {
+  // if user enters within the approximate diameter of the boo make it flee
+  if (d1 < boo.size) {
     if (!booLaugh.isPlaying()) {
       booLaugh.play();
     }
     if (dx < 0) {
       boo.vx = -boo.speed;
-    }
-    else if (dx > 0) {
+    } else if (dx > 0) {
       boo.vx = boo.speed;
     }
 
     if (dy < 0) {
       boo.vy = -boo.speed;
-    }
-    else if (dy > 0) {
+    } else if (dy > 0) {
       boo.vy = boo.speed;
     }
+    // if a boo leaves the bounds while you are within contact,
+    // simulation will assume that you were wbat pushed them out
     if (boo.x < 0 || boo.x > width || boo.y < 0 || boo.y > height) {
       state = `youMadeBooFloatAway`;
       spookyMusic.setVolume(0);
     }
   }
+  // if not in contact,
+  // have them act in accordance to progression through noise space
   else {
     boo.vx = map(noiseX, 0, 1, -boo.speed, boo.speed);
     boo.vy = map(noiseY, 0, 1, -boo.speed, boo.speed);
@@ -109,9 +125,11 @@ function moveBooAndCheckOffscreen(boo) {
   boo.y = boo.y + boo.vy;
 }
 
+// have the boos be darker the farther away they are from you
+// makes for an interesting flashlight like effect that is reminiscent of boo mechanics from actual mario games
 function displayBoo(boo) {
   let d2 = dist(mouseX, mouseY, boo.x, boo.y);
-  let shade = map(d2, 2*width/3, 0, 0, 255);
+  let shade = map(d2, (2 * width) / 3, 0, 0, 255);
   push();
   imageMode(CENTER);
   tint(shade, shade);
@@ -127,15 +145,18 @@ function moveAndDisplayUser() {
 
   let previousX = user.x;
 
+  // chnages direction based on the direction the mouse moves in
   user.xPositions.push(previousX);
   if (user.xPositions.length > user.maxPositions) {
     user.xPositions.shift();
   }
 
+  // I originally did this just with conditionals
+  // thank you Sam for the suggestion to use states!
+  // orientation is no longer affected by the order in which the conditionals were placed
   if (user.xPositions[0] < user.xPositions[1]) {
     user.direction = `left`;
-  }
-  else if (user.xPositions[0] > user.xPositions[1]) {
+  } else if (user.xPositions[0] > user.xPositions[1]) {
     user.direction = `right`;
   }
   if (user.direction === `left`) {
@@ -143,8 +164,7 @@ function moveAndDisplayUser() {
     imageMode(CENTER);
     image(invFeistyBoo, user.x, user.y, 415, 415);
     pop();
-  }
-  else if (user.direction === `right`) {
+  } else if (user.direction === `right`) {
     push();
     imageMode(CENTER);
     image(feistyBoo, user.x, user.y, 415, 415);
@@ -158,8 +178,12 @@ function title() {
   textSize(32);
   textAlign(CENTER, CENTER);
   fill(255);
-  text(`boos float away easy,
-  don't let em get lost!`, width/2, height/2);
+  text(
+    `boos float away easy,
+  don't let em get lost!`,
+    width / 2,
+    height / 2
+  );
   pop();
 }
 
@@ -180,8 +204,12 @@ function booFloatedAway() {
   textSize(32);
   textAlign(CENTER, CENTER);
   fill(255);
-  text(`a boo floated away,
-  don't let em get lost next time!`, width/2, height/2);
+  text(
+    `a boo floated away,
+  don't let em get lost next time!`,
+    width / 2,
+    height / 2
+  );
   pop();
 }
 
@@ -191,8 +219,12 @@ function youMadeBooFloatAway() {
   textSize(32);
   textAlign(CENTER, CENTER);
   fill(255);
-  text(`you made a boo float away,
-  why would you go and do such a thing?!?`, width/2, height/2);
+  text(
+    `you made a boo float away,
+  why would you go and do such a thing?!?`,
+    width / 2,
+    height / 2
+  );
   pop();
 }
 
@@ -201,14 +233,11 @@ function draw() {
 
   if (state === `title`) {
     title();
-  }
-  else if (state === `keepEmTogether`) {
+  } else if (state === `keepEmTogether`) {
     keepEmTogether();
-  }
-  else if (state === `booFloatedAway`) {
+  } else if (state === `booFloatedAway`) {
     booFloatedAway();
-  }
-  else if (state === `youMadeBooFloatAway`) {
+  } else if (state === `youMadeBooFloatAway`) {
     youMadeBooFloatAway();
   }
 }
