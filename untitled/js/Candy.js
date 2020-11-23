d7808068662dbeac1ba6ff49713d557f4c5dd9a3
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2020-11-22 19:44:03 -0500

    first draft with qr code

--- untitled/js/Candy.js
+++ untitled/js/Candy.js
@@ -1,5 +1,5 @@
 class Candy {
-  constructor(x, y, z, vx, vy, vz, id, others) {
+  constructor(x, y, z, vx, vy, vz, id, others, r, g, b) {
     this.modelOffset = -75;
     this.position = createVector(x, y, z);
     this.velocity = createVector(vx, vy, vz);
@@ -7,6 +7,9 @@ class Candy {
     this.w = 115;
     this.h = 30;
     this.d = 60;
+    this.r = r;
+    this.g = g;
+    this.b = b;
     this.id = id;
     this.others = others;
   }
@@ -20,20 +23,76 @@ class Candy {
     this.position.add(this.velocity);
   }
 
-  triangle() {
-    let area = abs( (width/tan(PI*30.0 / 180.0) - (-width/tan(PI*30.0 / 180.0))) *
-                    (-width/tan(PI*30.0 / 180.0)) - (width/2) );
-    let area1
+  lineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
+    let uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+    let uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+
+    if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+      return true;
+    }
+    return false;
+  }
+
+  lineRect(x1, y1, x2, y2, rx, ry, rw, rh) {
+    let left =   this.lineLine(x1,y1,x2,y2, rx,ry,rx, ry+rh);
+    let right =  this.lineLine(x1,y1,x2,y2, rx+rw,ry, rx+rw,ry+rh);
+    let top =    this.lineLine(x1,y1,x2,y2, rx,ry, rx+rw,ry);
+    let bottom = this.lineLine(x1,y1,x2,y2, rx,ry+rh, rx+rw,ry+rh);
+
+    if (left || right || top || bottom) {
+      return true;
+    }
+    return false;
+  }
+
+  checkOverlapLeftWall() {
+    let hit = this.lineRect(-width/tan(PI*30.0 / 180.0), width/2, 0, -width/tan(PI*30.0 / 180.0),
+                            this.position.x, this.position.z, this.w, this.d);
+    if (hit) {
+      this.velocity.x = -(this.velocity.x * 0.5);
+      this.velocity.z = -(this.velocity.z * 0.5);
+      return true;
+    }
+    return false;
+  }
+
+  checkOverlapRightWall() {
+    let hit = this.lineRect(width/tan(PI*30.0 / 180.0), width/2, 0, -width/tan(PI*30.0 / 180.0),
+                            this.position.x, this.position.z, this.w, this.d);
+    if (hit) {
+      this.velocity.x = -(this.velocity.x * 0.5);
+      this.velocity.z = -(this.velocity.z * 0.5);
+      return true
+    }
   }
 
   checkOverlapFloor() {
-    if (this.position.y + this.h/2 >= height/2 &&
-        this.position.x + this.w/2 > ) {
-      this.position.y = height/2 - this.h/2;
-      this.velocity.x = this.velocity.x * 0.25;
-      this.velocity.y = -(this.velocity.y * 0.5);
-      this.velocity.z = this.velocity.z * 0.25;
+    if (this.position.y + this.h/2 >= height/2) {
+      if (!this.checkOverlapLeftWall() || this.checkOverlapRightWall()) {
+        this.position.y = height/2 - this.h/2;
+        this.velocity.x = this.velocity.x * 0.25;
+        this.velocity.y = -(this.velocity.y * 0.5);
+        this.velocity.z = this.velocity.z * 0.25;
+        return true;
+      }
+      else if (this.checkOverlapLeftWall()) {
+        console.log(`bump`);
+        this.position.y = height/2 - this.h/2;
+        this.velocity.x = this.velocity.x + 1;
+        this.velocity.y = -(this.velocity.y * 0.5);
+        this.velocity.z = this.velocity.z + 1;
+        return true;
+      }
+      else if (this.checkOverlapRightWall()) {
+        console.log(`bump`);
+        this.position.y = height/2 - this.h/2;
+        this.velocity.x = this.velocity.x - 1;
+        this.velocity.y = -(this.velocity.y * 0.5);
+        this.velocity.z = this.velocity.z + 1;
+        return true;
+      }
     }
+    return false;
   }
 
   checkOverlapCandy() {
@@ -44,13 +103,18 @@ class Candy {
           this.position.y - this.h/2 < this.others[i].position.y + this.others[i].h/2 &&
           this.position.z + this.d/2 > this.others[i].position.z - this.others[i].d/2 &&
           this.position.z - this.d/2 < this.others[i].position.z + this.others[i].d/2) {
-        //console.log(`bump`);
-        let dx = this.position.x - this.others[i].position.x;
-        this.velocity.x = this.velocity.x + map(dx, -this.w/2, this.w/2, -1, 1);
-        let dy = this.position.y - this.others[i].position.y;
-        this.velocity.y = this.velocity.y + map(dy, -this.h/2, this.h/2, -1, 1);
-        let dz = this.position.z - this.others[i].position.z;
-        this.velocity.z = this.velocity.z + map(dz, -this.d/2, this.d/2, -1, 1);
+
+      if (!this.others[i].checkOverlapFloor()) {
+          let dx = this.position.x - this.others[i].position.x;
+          this.velocity.x = this.velocity.x + map(dx, -this.w/2, this.w/2, -1, 1);
+          let dy = this.position.y - this.others[i].position.y;
+          this.velocity.y = this.velocity.y + map(dy, -this.h/2, this.h/2, -1, 1);
+          let dz = this.position.z - this.others[i].position.z;
+          this.velocity.z = this.velocity.z + map(dz, -this.d/2, this.d/2, -1, 1);
+        }
+        else {
+          this.position.y = this.others[i].position.y - this.h/2;
+        }
       }
     }
   }
@@ -58,6 +122,8 @@ class Candy {
 
   display() {
     push();
+    noStroke();
+    fill(this.r, this.g, this.b);
     translate(0, this.modelOffset, 0);
     translate(this.position);
     model(candyModel);
--- untitled/js/script.js
+++ untitled/js/script.js
@@ -7,24 +7,29 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let qr;
 let candyModel;
 let candies = [];
-let numCandies = 5;
+let numCandies = 10;
 
 function preload() {
   // is about 87.5 (75?) pixels below where it should be
   // it's dimensions are 115, 30, 60
   candyModel = loadModel(`assets/images/candy.OBJ`);
+  qr = loadImage(`assets/images/frame.png`);
 }
 
 function setup() {
   createCanvas(750, 750, WEBGL);
-
+  //randomSeed(12);
   for (let i = 0; i < numCandies; i++) {
-    let x = random(-1, 1);
-    let y = random(-1, 1);
-    let z = random(-1, 1);
-    let candy = new Candy(x, -height/tan(PI*30.0 / 180.0) + y, z, 0, 1, 0, i, candies);
+    let x = random(-0.5, 0.5);
+    let y = random(-0.5, 0.5);
+    let z = random(-0.5, 0.5);
+    let r = random(200, 255);
+    let g = random(200, 255);
+    let b = random(200, 255);
+    let candy = new Candy(x, -height/tan(PI*30.0 / 180.0) + y, -width + z, 0, 0.5, 0, i, candies, r, g, b);
     candies.push(candy);
   }
 }
@@ -33,12 +38,16 @@ function draw() {
   background(255);
   orbitControl();
   drawRoom();
+  ambientLight(127);
+  directionalLight(255, 255, 255, 0, 1200, -750);
 
   for (let i = 0; i < candies.length; i++) {
     let candy = candies[i];
     let gravity = createVector(0, 2, 0);
     candy.gravity(gravity);
     candy.move();
+    candy.checkOverlapLeftWall();
+    candy.checkOverlapRightWall();
     candy.checkOverlapFloor();
     candy.checkOverlapCandy();
     candy.display();
@@ -48,13 +57,22 @@ function draw() {
 function platform() {
   push();
   beginShape();
-  vertex(-width/tan(PI*30.0 / 180.0), height/2, -width/tan(PI*30.0 / 180.0));
   vertex(-width/tan(PI*30.0 / 180.0), height/2, width/2);
+  vertex(0, height/2, -width/tan(PI*30.0 / 180.0));
   vertex(width/tan(PI*30.0 / 180.0), height/2, width/2);
-  vertex(width/tan(PI*30.0 / 180.0), height/2, -width/tan(PI*30.0 / 180.0));
   endShape(CLOSE);
   pop();
 }
+
+// function roof() {
+//   push();
+//   beginShape();
+//   vertex(-width/tan(PI*30.0 / 180.0), -height/tan(PI*30.0 / 180.0), width/2);
+//   vertex(0, -height/tan(PI*30.0 / 180.0), -width/tan(PI*30.0 / 180.0));
+//   vertex(width/tan(PI*30.0 / 180.0), -height/tan(PI*30.0 / 180.0), width/2);
+//   endShape(CLOSE);
+//   pop();
+// }
 
 function leftWall() {
   push();
@@ -82,4 +100,10 @@ function drawRoom() {
   platform();
   leftWall();
   rightWall();
+  push();
+  translate(0, height/2 - 1, 0);
+  rotateX(PI/2);
+  imageMode(CENTER);
+  image(qr, 0, 0);
+  pop();
 }
